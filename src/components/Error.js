0f4e29b75ee1f9842f31a413811e0c519b12ead1
Anthony Huerta <40744247+ajhuerta@users.noreply.github.com>
2021-11-05 15:38:18 -0500

    Add files via upload

---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Error = () => {
+    return(
+        <div className = 'showcase' style = {{height: '650px', display: 'flex', alignItems:
+        'center', color: '#fff', fontSize: '30px', backgroundColor: '#333', fontSize: '30px',
+        textAlign: 'center'}}>
+
+            <div className = 'overlay'>
+            <h1>Looks like this page doesn't exist</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Error
