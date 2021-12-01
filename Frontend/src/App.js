e28c4c56b5e899c33f9f823363fac9bec2390cc1
Kuotta <dsvid5140444@gmail.com>
2021-12-01 15:52:46 -0600

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+import Destinations from './components/Destinations'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Footer from './components/Footer'
+import Error from './components/Error'
+import UserDashboard from './components/UserDashboard'
+import DashboardHeader from './components/DashboardHeader'
+import UserPlans from './components/UserPlans'
+import UserActivityMatcher from './components/UserActivityMatcher'
+import UserManageAccount from './components/UserManageAccount'
+import SearchEngine from './components/SearchEngine'
+
+function App() {
+  return (
+    <Router>
+
+      <Switch>
+      <Route exact path= "/">
+          <Header/>
+          <Showcase/> 
+         <Destinations/> 
+         <Footer/>
+      </Route>
+
+      <Route path = "/login">
+        <Header/>
+        <Login/>
+        <Footer/>
+      </Route>
+
+      <Route path = '/signup'>
+        <Header/>
+        <SignUp/>
+        <Footer/>
+      </Route>
+
+      <Route path = '/profile'>
+        <DashboardHeader/>
+        <UserDashboard/>
+      </Route>
+
+      <Route path = '/my-plans'>
+        <DashboardHeader/>
+        <UserPlans/>
+      </Route>
+
+      <Route path = '/park-activities'>
+        <DashboardHeader/>
+        <UserActivityMatcher/>
+      </Route>
+
+      <Route path = '/search-engine'>
+        <DashboardHeader/>
+        <SearchEngine/>
+      </Route>
+
+      <Route path = '/manage-account'>
+        <DashboardHeader/>
+        <UserManageAccount/>
+        
+      </Route>
+
+      <Route path = "*">
+        <Header/>
+        <Error/>
+        <Footer/>
+      </Route>
+
+      </Switch>
+
+    </Router>
+
+
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className = "footer">
+            <ul>
+                <li><button>Home</button></li>
+                <li><button>Privacy Policy</button></li>
+            </ul>
+
+            <ul>
+                <li><button>About Us</button></li>
+                <li><button>Privacy Policy</button></li>
+            </ul>
+
+
+            <ul>
+                <li><button>Random</button></li>
+                <li><button>Giveaway</button></li>
+            </ul>
+
+        </footer>
+    )
+}
+
+export default Footer
